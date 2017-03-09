4e7a23c845550617aa5b5b63d55f78aed0a5b70d
vagrant <vagrant@homestead>
2017-03-09 17:24:08 +0000

    commit

--- config/mail.php
+++ config/mail.php
@@ -86,7 +86,7 @@ return [
 
     'username' => env('MAIL_USERNAME', 'yourdaddiness@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD', 'vpqyngslrmfeuhvv'),
+    'password' => env('MAIL_PASSWORD', 'aotqasjrjcpjkzkg'),
 
     /*
     |--------------------------------------------------------------------------
